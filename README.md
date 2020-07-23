b4b634ae2506772b02235b07703690551fd95493
sebastianloschert <sebastian.loschert@gmx.de>
2020-07-23 16:09:18 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+**Magic cooking recipe recommender**
+
+Just a usefull recipe recommender: Tell me what you (still) have in your fridge and I
+tell you what you can cook with it.
+
+Build with Flask, deployed on Heroku.
+
+The recipes and ingredientes are originally from the website Koch-Wiki (https://www.kochwiki.org/).
---
+++ flask_app/application.py
@@ -0,0 +1,53 @@
+import os
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask_app.src.recipe_recommender import recipe_recommender
+import json
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+
+    most_used_ingredients = []
+    list_of_ingredients = []
+
+    with open("flask_app/data/most_used_ingredients.txt", "r") as f:
+        for line in f:
+            most_used_ingredients.append(line.strip())
+
+    with open("flask_app/data/list_of_ingredients.txt", "r") as f:
+        for line in f:
+            list_of_ingredients.append(line.strip())
+
+    return render_template('index.html', most_used_ingredients=most_used_ingredients, list_of_ingredients=list_of_ingredients)
+
+@app.route('/results')
+def results():
+    user_input = dict(request.args)
+    user_input_values = list(user_input.values()) #   list(user_input.values())
+    print("user_input_values", user_input_values)
+    # unpack the multiple inputs recieved by the jQuery input-fields
+    u1 = [i.split(",") for i in user_input_values]
+    print("u1", u1)
+    u2 = []
+    for i in u1:
+        for j in i:
+            u2.append(j)
+    # drop empty entry
+    try:
+        u2.remove("")
+        u2.remove("")
+    except:
+        pass
+    print("u2", u2)
+    user_ingredients = set(u2)
+    print("user_ingredients:", user_ingredients)
+
+    recommendations = recipe_recommender(user_ingredients)
+    #print(recommendations)
+    return render_template('results.html', recommendations=recommendations, user_ingredients=list(user_ingredients))
+
+if __name__== '__main__':
+    app.debug = True
+    app.run(host='127.0.0.1', port='5000')
