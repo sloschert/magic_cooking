bd7e07bcc196dd5589636e8b0441a6ba086f86af
sebastianloschert <sebastian.loschert@gmx.de>
2020-08-02 17:12:36 +0200

    minor in application.py

--- flask_app/application.py
+++ flask_app/application.py
@@ -3,28 +3,22 @@ from flask import Flask, render_template, request, redirect, url_for, send_from_
 from flask_app.src.recipe_recommender import recipe_recommender
 import json
 
-
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 @app.route('/chose_ingredients')
 def chose_ingredients():
-
     most_used_ingredients = []
     list_of_ingredients = []
-
     with open("flask_app/data/most_used_ingredients.txt", "r") as f:
         for line in f:
             most_used_ingredients.append(line.strip())
-
     with open("flask_app/data/list_of_ingredients.txt", "r") as f:
         for line in f:
             list_of_ingredients.append(line.strip())
-
     return render_template('chose_ingredients.html', most_used_ingredients=most_used_ingredients, list_of_ingredients=list_of_ingredients)
 
 @app.route('/results')
@@ -45,7 +39,6 @@ def results():
         pass
     user_ingredients = set(u2)
     recommendations = recipe_recommender(user_ingredients)
-    #print(recommendations)
     return render_template('results.html', recommendations=recommendations, user_ingredients=list(user_ingredients))
 
 if __name__== '__main__':
